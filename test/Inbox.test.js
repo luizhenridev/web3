4230ae906caf0d8923db63fff03e5a256b99aa39
Luiz Cavalcante <luiz.cavalcante@ifood.com.br>
2024-04-29 01:33:21 -0300

    first commit

---
+++ test/Inbox.test.js
@@ -0,0 +1,7 @@
+const ganache = require('ganache');
+const { Web3 } = require('web3');
+const assert = require('assert')
+const web3 = new Web3(ganache.provider())
+
+// updated ganache and web3 imports added for convenience
+// contract test code will go here
