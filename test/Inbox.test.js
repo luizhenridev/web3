c7213a2c302a90f99363a41a5a2fd9b19c15f0cf
Luiz Cavalcante <luiz.cavalcante@ifood.com.br>
2024-04-30 18:45:10 -0300

    tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,24 @@
 const ganache = require('ganache');
-const { Web3 } = require('web3');
-const assert = require('assert')
-const web3 = new Web3(ganache.provider())
+const Web3  = require('web3');
+const assert = require('assert');
+const web3 = new Web3(ganache.provider());
 
-// updated ganache and web3 imports added for convenience
-// contract test code will go here
+
+class Car {
+    park(){
+        return 'stopped';
+    }
+
+    drive(){
+        return 'vroom';
+    }
+}
+
+
+describe('Car', ()=>{
+    it('can park',()=>{
+        const car = new Car();
+        assert.equal(car.park(), 'stopped');
+    });
+
+});
