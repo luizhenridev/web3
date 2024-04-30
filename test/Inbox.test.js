3aec3e498c1cc1cb7f6ac6743a9ea823feb596d7
Luiz Cavalcante <luiz.cavalcante@ifood.com.br>
2024-04-30 19:41:52 -0300

    tes

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,24 +1,23 @@
 const ganache = require('ganache');
-const Web3  = require('web3');
+const {Web3}  = require('web3');
 const assert = require('assert');
 const web3 = new Web3(ganache.provider());
 
 
-class Car {
-    park(){
-        return 'stopped';
-    }
 
-    drive(){
-        return 'vroom';
-    }
-}
+beforeEach(()=>{
+    //Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts =>{
+            console.log(fetchedAccounts);
+        });
+
+    //Use one of those accounts to deploy 
+
+    //the contract
+});
 
 
-describe('Car', ()=>{
-    it('can park',()=>{
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
-    });
-
+describe('Inbox', ()=>{
+    it('deploys a contract', () => {});
 });
